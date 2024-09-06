dd697e19ae9403f0a9911b0453b5fb81c3bb1650
B0G0N1 <michelebogoni2001@gmail.com>
2024-09-06 02:25:54 +0200

    [FEAT] Create index and print hotel list

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+// Array di hotel
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
+
+// Ciclo per stampare i dati degli hotel
+foreach ($hotels as $hotel) {
+    echo "<h2>{$hotel['name']}</h2>";
+    echo "<p>Description: {$hotel['description']}</p>";
+    echo "<p>Parking: " . ($hotel['parking'] ? 'Yes' : 'No') . "</p>";
+    echo "<p>Vote: {$hotel['vote']}</p>";
+    echo "<p>Distance to Center: {$hotel['distance_to_center']} km</p>";
+    echo "<hr>";
+}
+
+?>
